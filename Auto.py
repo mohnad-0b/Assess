87be64a9b4bcb1b2ef770922c31e01be5bc7aac4
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2022-06-18 13:36:45 +0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -1,16 +1,16 @@
-from base64 import encode
+from time import sleep
 from selenium import webdriver 
 from webdriver_manager.chrome import ChromeDriverManager
-import requests
-import os
 
-username = input("username : ") 
-password = input("password : ")
-natno = input("natno : ") # الرقم الوطني
-n = int(input("number of cours : ")) # عدد المواد
+username = "" # your username
+password = '' # Password
+natno = "" # the ID number
+n = 5 # number of corses
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://app2.bau.edu.jo:7799/eval/Login.jsp")
+
+# login 
 
 browser.find_element_by_name("tbstdno").send_keys(username)
 browser.find_element_by_name("tbstdpass").send_keys(password)
@@ -18,10 +18,9 @@ browser.find_element_by_name("tbstdnatno").send_keys(natno)
 browser.find_element_by_css_selector("input[type=\"submit\" i]").click()
 browser.implicitly_wait(100)
 
-for i in range(1,n):
-    
-    for i in range(1,20):
-        browser.get("https://app2.bau.edu.jo:7799/eval/Evaluation.jsp?qno="+str(i))
+for i in range(n):
+    for j in range(1,20):
+        browser.get("https://app2.bau.edu.jo:7799/eval/Evaluation.jsp?qno="+str(j))
         browser.find_elements_by_xpath("//input[@name='evalans' and @value='1']")[0].click()
 
     pageSource = browser.page_source
@@ -36,5 +35,5 @@ for i in range(1,n):
     for index,line in enumerate(file):
         if(index in lines):
             browser.find_element_by_name("captcha").send_keys(line)
-            browser.find_elements_by_xpath("input:not([type=\"image\" i])").click()
             break
+    sleep(5)
