eb21cffd610dfb7034b4a6ab460c54d3c4cf4510
mohnad banat <75941340+mohnad-0b@users.noreply.github.com>
2023-06-22 05:48:57 +0300

    Update Auto.py

--- Auto.py
+++ Auto.py
@@ -1,11 +1,12 @@
 from time import sleep
 from selenium import webdriver 
 from webdriver_manager.chrome import ChromeDriverManager
+import os
 
-username = "" # your username
+username = "" # ID university
 password = '' # Password
 natno = "" # the ID number
-n = 5 # number of course
+n = 0 # number of course
 
 browser = webdriver.Chrome(ChromeDriverManager().install())
 browser.get("https://app2.bau.edu.jo:7799/eval/Login.jsp")
@@ -29,11 +30,11 @@ for i in range(n):
     f.write(pageSource)
     f.close()
     
-    file = open("PageSource2","r+",encoding="utf8")
-    lines = [387]
-    
-    for index,line in enumerate(file):
-        if(index in lines):
-            browser.find_element_by_name("captcha").send_keys(line)
-            break
+    file = open("PageSource2","r+",encoding="utf8").read().split("\n")
+    captcha = ''.join(file[770-1].split(" ")).replace("\t","")    
+
+    browser.find_element_by_name("captcha").send_keys(captcha)
+    browser.find_element_by_css_selector("input[type=\"SUBMIT\" i]").click()
     sleep(5)
+
+os.remove("PageSource2")
